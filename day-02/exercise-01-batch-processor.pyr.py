11b189c123776fc005b1f79141616bb40e5deec1
Sudha-Shakya <sudhasakriya@gmail.com>
2026-08-13 16:26:59 +0545

    Update exercise-01-batch-processor.pyr.py

--- day-02/exercise-01-batch-processor.pyr.py
+++ day-02/exercise-01-batch-processor.pyr.py
@@ -1,1 +1,9 @@
+"Exercise 1: Batch Processor"
 
+# Use a for loop and range() to print batch numbers from 1 to 10
+for batch_number in range (1, 11):
+  print(f"Processing batch {batch_number}")
+
+# Modulo operator
+  if batch_number % 3 == 0:
+    print("Checkpoint_reached")
